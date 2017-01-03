d81d57c88140f9944f2945ccd4347080167ed922
KilluaHatake <chinturocks4u@gmail.com>
2017-01-03 23:56:12 +0530

    Update du.mk

--- du.mk
+++ du.mk
@@ -16,7 +16,7 @@ $(call inherit-product, $(SRC_EVERVOLV_DIR)/config/apns.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from oneplus2 device
 $(call inherit-product, device/oneplus/oneplus2/device.mk)
